dc9c88effa1098024525421d84d2d9de8d4607bd
mahhov <mahhov1@gmail.com>
2018-02-17 14:26:34 -0800

    differentiate between injection methods and injection fields

--- src/app/Generator.ts
+++ src/app/Generator.ts
@@ -6,17 +6,21 @@ export class Generator {
 
   public generateTemplate(fileContents: String): string {
     let declarationsRegexp = /function\((.*)\)/;
-    let invocationsRegexpTemplate = '{0}{1}\\.\\w*';
+    let invocationsRegexpTemplate = '{0}{1}\\.\\w*\\(';
     let methodRegexp = /\.(\w*)/;
     let commaListTemplate = '{0}, {1}';
     let newlineListTemplate = '{0}\n{1}';
     let varDeclarationTemplate = 'var {0};';
     let quoteTemplate = "'{0}'";
-    let provideBodyTemplate = "\t$provide.value('{0}', {0}Spy = jasmine.createSpyObj('{0}', [{1}]));";
+    let provideBodyTemplate = "\t$provide.value('{0}', {0} = jasmine.createSpyObj('{0}', [{1}]));";
+    let provideBodyConstTemplate = "\t$provide.value('{0}', {0} = {});";
     let providePrefix = 'beforeEach(module(function($provide) {';
     let provideSuffix = '}));';
     let provideTemplate = '{0}\n{1}\n{2}';
     let testTemplate = '{0}\n\n{1}';
+
+    if (!fileContents)
+      return;
 
     let declaration = fileContents.match(declarationsRegexp);
 
@@ -31,7 +35,7 @@ export class Generator {
       let regexp = RegExp(invocationsRegexpTemplate.formatUnicorn(injection.name[0] === '$' ? '\\' : '', injection.name), 'g');
       let invocations = fileContents.match(regexp);
       injection.methods = _.map(invocations, invocation => {
-        return invocation.match(methodRegexp)[1]
+        return invocation.match(methodRegexp)[1];
       });
     });
 
@@ -45,9 +49,11 @@ export class Generator {
       let quotedMethods = _.map(injection.methods, method => {
         return quoteTemplate.formatUnicorn(method);
       });
-      let methodList = quotedMethods.length ? _.reduce(quotedMethods, (aggregate, method) => {
+      if (!quotedMethods.length)
+        return provideBodyConstTemplate.formatUnicorn(injection.name);
+      let methodList = _.reduce(quotedMethods, (aggregate, method) => {
         return commaListTemplate.formatUnicorn(aggregate, method);
-      }) : '';
+      });
       return provideBodyTemplate.formatUnicorn(injection.name, methodList);
     });
     let provideBody = _.reduce(provideBodyLines, (aggregate, line) => {
