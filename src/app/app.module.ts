6a8c611a15d98ee3266bd5e92e35359ffb75a6a3
mahhov <mahhov1@gmail.com>
2018-02-19 12:32:04 -0800

    extract service for providign default text input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {App} from "./app";
+import {DefaultContentProvider} from "./DefaultContentProvider";
 import {Generator} from "./Generator";
 
 @NgModule({
@@ -13,6 +14,7 @@ import {Generator} from "./Generator";
     FormsModule
   ],
   providers: [
+    DefaultContentProvider,
     Generator
   ],
   bootstrap: [App]
